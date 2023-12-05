0f65b966376d6f80fd59ed6402cb7bbbe4573ba1
willqa01 <willqa01@gmail.com>
2023-12-05 10:50:58 +0100

    test selenium ok

--- testauto.py
+++ testauto.py
@@ -19,102 +19,166 @@ else :
 driver.get(url)
 
 testTableau = [
-    ["admin", "123456","admin", "987654","",""],
+    ["admin", "123456","987654","","","test user","test@example.com","test123","Sombre","Clair","Petite","Moyenne","Grande","Arrondi","Carré","Défaut","Probleme de connexion","impossible de se connecter"],
     ]
 
-#try:
-
 for i in testTableau:
-    #tc01/02/03/05
-    driver.find_element(By.ID, "username").send_keys(i[0])
-    driver.find_element(By.ID, "password").send_keys(i[1])
-    driver.find_element(By.ID, "login-form").click()
-    time.sleep(2)
-    driver.get("http://localhost/projet autonome/Projet-de-Test-Fonctionnel/index.html")
-    time.sleep(1)
-    #tc04/06
-driver.get("http://localhost/projet autonome/Projet-de-Test-Fonctionnel/index.html")
-time.sleep(3)
-driver.find_element(By.ID, "username").send_keys(i[0])   
-driver.find_element(By.ID, "password").send_keys(i[1])
-driver.find_element(By.ID, "login-form").find_element(By.CSS_SELECTOR, ".btn-login").click()
-time.sleep(2)
+    #tc01
+    try:
 
+        driver.find_element(By.ID, "username").send_keys(i[0])
+        driver.find_element(By.ID, "password").send_keys(i[1])
+        driver.find_element(By.ID, "login-form").click()
+        time.sleep(2)
+        driver.get("http://localhost/projet autonome/Projet-de-Test-Fonctionnel/index.html")
+        time.sleep(1)
+        with open(log_file, 'a', encoding="UTF-8") as file:   #'w' pour effacre le dossier log
+            file.write(f'reussite lors du test avec{i[0]} à {time.ctime()} - Succès :test validé \n ')
+    except Exception as e:
+        with open(log_file, 'a') as file:
+            file.write(f'erruer lors du test avec{i[3]} à {time.ctime()} : test erreur\n ')
+        print("An exception occurred")
+
+        #tc02
+    try:
+
+        driver.find_element(By.ID, "username").send_keys(i[0])
+        driver.find_element(By.ID, "password").send_keys(i[2])
+        driver.find_element(By.ID, "login-form").click()
+        time.sleep(2)
+        driver.get("http://localhost/projet autonome/Projet-de-Test-Fonctionnel/index.html")
+        time.sleep(1)
+        with open(log_file, 'a', encoding="UTF-8") as file:   #'w' pour effacre le dossier log
+            file.write(f'reussite lors du test avec{i[0]} à {time.ctime()} - Succès :test validé \n ')
+    except Exception as e:
+        with open(log_file, 'a') as file:
+            file.write(f'erruer lors du test avec{i[3]} à {time.ctime()} : test erreur\n ')
+            print("An exception occurred")
+
+            #tc03
+    try:
+
+        driver.find_element(By.ID, "username").send_keys(i[3])
+        driver.find_element(By.ID, "password").send_keys(i[4])
+        driver.find_element(By.ID, "login-form").click()
+        time.sleep(2)
+        driver.get("http://localhost/projet autonome/Projet-de-Test-Fonctionnel/index.html")
+        time.sleep(1)
+        with open(log_file, 'a', encoding="UTF-8") as file:   #'w' pour effacre le dossier log
+            file.write(f'reussite lors du test avec{i[0]} à {time.ctime()} - Succès :test validé \n ')
+    except Exception as e:
+        with open(log_file, 'a') as file:
+            file.write(f'erruer lors du test avec{i[3]} à {time.ctime()} : test erreur\n ')
+        print("An exception occurred")
+
+                #tc05
+    
+    try:
+
+        
+            id = driver.find_element(By.ID, "username").send_keys(i[0])
+            mdp = driver.find_element(By.ID, "password").send_keys(i[3])
+            driver.find_element(By.ID, "login-form").click()
+            time.sleep(2)
+            driver.get("http://localhost/projet autonome/Projet-de-Test-Fonctionnel/index.html")
+            time.sleep(1)
+            
+            with open(log_file, 'a', encoding="UTF-8") as file:   #'w' pour effacre le dossier log
+                file.write(f'reussite lors du test avec{i[3]} à {time.ctime()} - Succès :test validé \n ')
+    except Exception as e:
+        with open(log_file, 'a') as file:
+            file.write(f'erruer lors du test avec{i[1]} à {time.ctime()} : test erreur\n ')
+        print("An exception occurred")
+
+
+    #tc04/06
+    try:
+        driver.get("http://localhost/projet autonome/Projet-de-Test-Fonctionnel/index.html")
+        time.sleep(3)
+        driver.find_element(By.ID, "username").send_keys(i[0])   
+        driver.find_element(By.ID, "password").send_keys(i[1])
+        driver.find_element(By.ID, "login-form").find_element(By.CSS_SELECTOR, ".btn-login").click()
+        time.sleep(2)
+        with open(log_file, 'a', encoding="UTF-8") as file:   #'w' pour effacre le dossier log
+            file.write(f'reussite lors du test avec{i[0]} à {time.ctime()} - Succès :test validé \n ')
+    except Exception as e:
+        with open(log_file, 'a') as file:
+            file.write(f'erruer lors du test avec{i[0]} à {time.ctime()} : test erreur\n ')
+        print("An exception occurred")
 
     #tc07
-# driver.find_element(By.ID, "username").send_keys(i[0])   
-# driver.find_element(By.ID, "password").send_keys(i[1])
-# driver.find_element(By.ID, "login-form").find_element(By.CSS_SELECTOR, ".btn-login").click()
-# time.sleep(1)
-driver.find_element(By.ID, "link-users").click()
-driver.find_element(By.CSS_SELECTOR, ".btn-success").click()
-time.sleep(1)
-driver.find_element(By.ID, "username").send_keys("test user")
-driver.find_element(By.ID, "email").send_keys("test@example.com")
-driver.find_element(By.ID, "password").send_keys("test123")
-driver.find_element(By.CSS_SELECTOR, ".btn-primary").click()
-time.sleep(1)
-# driver.get("http://localhost/projet autonome/Projet-de-Test-Fonctionnel/index.html")
-time.sleep(1)
-    
-    #tc08  
-# driver.find_element(By.ID, "username").send_keys(i[0])   
-# driver.find_element(By.ID, "password").send_keys(i[1])
-# driver.find_element(By.ID, "login-form").find_element(By.CSS_SELECTOR, ".btn-login").click()
-# time.sleep(1)
-driver.find_element(By.ID, "link-settings").click()
-couleur = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "theme-selector")))
-select = Select(couleur)
-select.select_by_visible_text("Sombre")
-police = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "font-size-selector")))
-select = Select(police)
-select.select_by_visible_text("Grande")
-couleur = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "border-style-selector")))
-select = Select(couleur)
-select.select_by_visible_text("Arrondi")
-# driver.get("http://localhost/projet autonome/Projet-de-Test-Fonctionnel/index.html")
-time.sleep(1)
+    try:
+        driver.find_element(By.ID, "link-users").click()
+        driver.find_element(By.CSS_SELECTOR, ".btn-success").click()
+        time.sleep(1)
+        driver.find_element(By.ID, "username").send_keys(i[5])
+        driver.find_element(By.ID, "email").send_keys(i[6])
+        driver.find_element(By.ID, "password").send_keys(i[7])
+        driver.find_element(By.CSS_SELECTOR, ".btn-primary").click()
+        time.sleep(1)
+        with open(log_file, 'a', encoding="UTF-8") as file:   #'w' pour effacre le dossier log
+            file.write(f'reussite lors du test avec{i[0]} à {time.ctime()} - Succès :test validé \n ')
+    except Exception as e:
+        with open(log_file, 'a') as file:
+            file.write(f'erruer lors du test avec{i[0]} à {time.ctime()} : test erreur\n ')
+        print("An exception occurred")
+
+    #tc08 
+    try: 
+        driver.find_element(By.ID, "link-settings").click()
+        couleur = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "theme-selector")))
+        select = Select(couleur)
+        select.select_by_visible_text(i[8])
+        police = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "font-size-selector")))
+        select = Select(police)
+        select.select_by_visible_text(i[12])
+        couleur = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "border-style-selector")))
+        select = Select(couleur)
+        select.select_by_visible_text(i[13])
+        time.sleep(1)
+        with open(log_file, 'a', encoding="UTF-8") as file:   #'w' pour effacre le dossier log
+            file.write(f'reussite lors du test avec{i[0]} à {time.ctime()} - Succès :test validé \n ')
+    except Exception as e:
+        with open(log_file, 'a') as file:
+            file.write(f'erruer lors du test avec{i[0]} à {time.ctime()} : test erreur\n ')
+        print("An exception occurred")
 
     #tc09
-# driver.find_element(By.ID, "username").send_keys(i[0])   
-# driver.find_element(By.ID, "password").send_keys(i[1])
-# driver.find_element(By.ID, "login-form").find_element(By.CSS_SELECTOR, ".btn-login").click()
-# time.sleep(1)
-driver.find_element(By.ID, "link-analytics").click()
-time.sleep(1)
-# driver.get("http://localhost/projet autonome/Projet-de-Test-Fonctionnel/index.html")
-# time.sleep(1)
+    try:
+        driver.find_element(By.ID, "link-analytics").click()
+        time.sleep(1)
+        with open(log_file, 'a', encoding="UTF-8") as file:   #'w' pour effacre le dossier log
+            file.write(f'reussite lors du test avec{i[0]} à {time.ctime()} - Succès :test validé \n ')
+    except Exception as e:
+        with open(log_file, 'a') as file:
+            file.write(f'erruer lors du test avec{i[0]} à {time.ctime()} : test erreur\n ')
+        print("An exception occurred")
 
     #tc10
-# driver.find_element(By.ID, "username").send_keys(i[0])   
-# driver.find_element(By.ID, "password").send_keys(i[1])
-# driver.find_element(By.ID, "login-form").find_element(By.CSS_SELECTOR, ".btn-login").click()
-# time.sleep(1)   
-driver.find_element(By.ID, "link-support").click()
-driver.find_element(By.CSS_SELECTOR, ".btn-success").click()
-driver.find_element(By.ID, "title").send_keys("Probleme de connexion")
-driver.find_element(By.ID, "description").send_keys("impossible de se connecter")
-driver.find_element(By.CSS_SELECTOR, ".btn-primary").click()
-time.sleep(1)
-# driver.get("http://localhost/projet autonome/Projet-de-Test-Fonctionnel/index.html")
-# time.sleep(2)
+    try:
+        driver.find_element(By.ID, "link-support").click()
+        driver.find_element(By.CSS_SELECTOR, ".btn-success").click()
+        driver.find_element(By.ID, "title").send_keys(i[16])
+        driver.find_element(By.ID, "description").send_keys(i[17])
+        driver.find_element(By.CSS_SELECTOR, ".btn-primary").click()
+        time.sleep(1)
+        with open(log_file, 'a', encoding="UTF-8") as file:   #'w' pour effacre le dossier log
+            file.write(f'reussite lors du test avec{i[0]} à {time.ctime()} - Succès :test validé \n ')
+    except Exception as e:
+        with open(log_file, 'a') as file:
+            file.write(f'erruer lors du test avec{i[0]} à {time.ctime()} : test erreur\n ')
+        print("An exception occurred")
 
     #tc11
-# driver.find_element(By.ID, "username").send_keys(i[0])   
-# driver.find_element(By.ID, "password").send_keys(i[1])
-# driver.find_element(By.ID, "login-form").find_element(By.CSS_SELECTOR, ".btn-login").click()
-# time.sleep(2) 
-driver.find_element(By.LINK_TEXT,"Déconnexion").click()
-time.sleep(2)
-
-
-
-#     with open(log_file, 'a', encoding="UTF-8") as file:   #'w' pour effacre le dossier log
-#             file.write(f'reussite lors du test avec{i[0]} à {time.ctime()} - Succès :test validé \n ')
-# except Exception as e:
-#     with open(log_file, 'a') as file:
-#             file.write(f'erruer lors du test avec{i[0]} à {time.ctime()} : test erreur\n ')
-#             print("An exception occurred")
+    try: 
+        driver.find_element(By.LINK_TEXT,"Déconnexion").click()
+        time.sleep(2)
+        with open(log_file, 'a', encoding="UTF-8") as file:   #'w' pour effacre le dossier log
+            file.write(f'reussite lors du test avec{i[0]} à {time.ctime()} - Succès :test validé \n ')
+    except Exception as e:
+        with open(log_file, 'a') as file:
+            file.write(f'erruer lors du test avec{i[0]} à {time.ctime()} : test erreur\n ')
+        print("An exception occurred")
 
 
 
